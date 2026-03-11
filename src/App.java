ca6719616ffcfe55a6a46c267c87bafcf6f88dc4
gustavo santos <gustavo.dubsantos@gmail.com>
2026-03-10 21:16:53 -0300

    chae rebolando gostoso pra staff toda

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] argss) throws Exception {
         
-
             Nxr motoca = new Nxr("yamaha", "anxr 160", 2019, 12, "asd", "trilha");
             System.out.println(motoca);
     }
